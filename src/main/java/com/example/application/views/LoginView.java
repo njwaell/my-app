a1b59a6ab4ce9ec6e9d52fb47d01d7a76faf992c
njwaell <njwaeel@gmail.com>
2023-11-16 14:38:02 +0700

    Adding form Category

---
+++ src/main/java/com/example/application/views/LoginView.java
@@ -0,0 +1,27 @@
+package com.example.application.views;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+
+@Route("Login")
+@PageTitle("LOGIN")
+@AnonymousAllowed
+
+public class LoginView extends VerticalLayout{
+    private static final LoginForm login = new LoginForm();
+    public LoginView(){
+        addClassName("login-view");
+        setSizeFull();
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+
+        login.setAction("login");
+
+        add(new H1("Login Page"),login);
+}
+}
--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -2,6 +2,7 @@ package com.example.application.views;
 
 import com.example.application.views.about.AboutView;
 import com.example.application.views.helloworld.HelloWorldView;
+import com.example.application.views.report.CreateReportFormView;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.Footer;
@@ -53,6 +54,7 @@ public class MainLayout extends AppLayout {
 
         nav.addItem(new SideNavItem("Hello World", HelloWorldView.class, LineAwesomeIcon.GLOBE_SOLID.create()));
         nav.addItem(new SideNavItem("About", AboutView.class, LineAwesomeIcon.FILE.create()));
+        nav.addItem(new SideNavItem("Layanan Pengaduan", CreateReportFormView.class, LineAwesomeIcon.RASPBERRY_PI.create()));
 
         return nav;
     }
