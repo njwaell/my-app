157e499aa8bf47b08e1787eb927003b158240867
njwaell <njwaeel@gmail.com>
2023-11-16 14:54:09 +0700

    Adding form category

---
+++ src/main/java/com/example/application/model/Category.java
@@ -0,0 +1,21 @@
+package com.example.application.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "category_id")
+    private long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+}
